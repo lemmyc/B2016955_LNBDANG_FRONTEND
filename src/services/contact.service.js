f085b2318d9ddb4ce1df3c55cb01d7bb2d657ff8
lemmyc <mrburgundy2911@gmail.com>
2023-10-09 01:37:28 +0700

    Tao trang cap nhat lien he

--- src/services/contact.service.js
+++ src/services/contact.service.js
@@ -16,7 +16,7 @@ class ContactService{
     async get(id){
         return (await this.api.get(`/${id}`)).data;
     }
-    async get(id, data){
+    async update(id, data){
         return (await this.api.put(`/${id}`, data)).data;
     }
     async delete(id){
