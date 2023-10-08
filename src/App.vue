fd2d197eeec33ae7b4b43462bfc5d49110c644ae
lemmyc <mrburgundy2911@gmail.com>
2023-10-08 14:53:40 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+  export default {
+
+  }
+</script>
+
+<template>
+  <header>
+    <h1>Hello, Vue.JS</h1>
+  </header>
+</template>
+
+<style>
+  .page{
+    max-width: 400px;
+    margin: auto;
+  }
+</style>
